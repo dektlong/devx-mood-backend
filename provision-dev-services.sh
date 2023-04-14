47c1961727c404ac810e6ebdd24002b4e64986ca
dekel <dekel@vmware.com>
2023-04-13 17:31:30 -0700

    add data services

--- provision-dev-services.sh
+++ provision-dev-services.sh
@@ -1,3 +1,13 @@
-tanzu service class-claim create inventory --class mysql-unmanaged --parameter storageGB=1 -n YOUR_DEV_NAMESPACE
+#!/usr/bin/env bash
 
-tanzu service class-claim create reading --class rabbitmq-unmanaged --parameter replicas=1 --parameter storageGB=1 -n YOUR_DEV_NAMESPACE
+dev_namespace=$1
+
+if [ "$dev_namespace" == "" ] 
+then
+    echo "Error!! please specific developer namespace"
+    exit
+fi
+
+tanzu service class-claim create inventory --class mysql-unmanaged --parameter storageGB=1 -n $dev_namespace
+
+tanzu service class-claim create reading --class rabbitmq-unmanaged --parameter replicas=1 --parameter storageGB=1 -n $dev_namespace
