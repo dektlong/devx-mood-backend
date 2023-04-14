c56f42bcd81724b9296db3e2b42070790df11a02
dekel <dekel@vmware.com>
2023-04-13 19:07:52 -0700

    modify provision dev services script  for postgresql

--- provision-dev-services.sh
+++ provision-dev-services.sh
@@ -8,6 +8,6 @@ then
     exit
 fi
 
-tanzu service class-claim create inventory --class mysql-unmanaged --parameter storageGB=1 -n $dev_namespace
+tanzu service class-claim create inventory --class postgresql-unmanaged --parameter storageGB=1 -n $dev_namespace
 
 tanzu service class-claim create reading --class rabbitmq-unmanaged --parameter replicas=1 --parameter storageGB=1 -n $dev_namespace
